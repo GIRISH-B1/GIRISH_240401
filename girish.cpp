19e109e9396527ee1746d1dfd559a871ca6cb266
Girish <girishbhojwani3106@gmail.com>
2026-06-01 21:50:42 +0530

    Add files via upload

--- girish.cpp
+++ girish.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int main(){
+    vector<int> vec;
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(3);
+    cout<<vec.at(2);
+
+}
+    
